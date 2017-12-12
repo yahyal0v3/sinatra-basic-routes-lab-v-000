577934b54c3c798f13151d775a6b3fcf8a8ee350
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-12 01:41:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get ('/') {"Hello, World!"}
+  get ('/name') {"My name is Khaya."}
+  get ('/hometown') {"My hometown is Teaneck."}
+  get ('/favorite-song') {"My favorite song is 'Just For Now'."}
 end
